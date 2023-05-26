ad49b719cf4d5b602bee857eaa544dfbff278de6
amadeus <amadeus@1token.trade>
2023-05-26 15:54:52 +0800

    feat: update main page

--- application/web/src/views/mainPage/list/index.vue
+++ application/web/src/views/mainPage/list/index.vue
@@ -10,10 +10,36 @@
         项目说明
       </div>
 
-      <div class="mainPageNote" style="float: left;">
-        <el-tag>说明示例: </el-tag>
-        <span style="margin-left: 5px;"> {{ 'amadeus' }}</span>
-      </div>
+      <div class="mainPageNote">
+        <el-tag>系统角色: </el-tag>
+        <span style="margin-left: 5px;"> {{ '管理员、医生、患者、药店、保险机构' }}</span>
+      </div><br>
+
+      <div class="mainPageNote">
+        <el-tag type="success">医生功能: </el-tag>
+        <span style="margin-left: 5px;"> {{ '查看患者信息、创建病历、查看病历' }}</span>
+      </div><br>
+
+      <div class="mainPageNote">
+        <el-tag type="warning">患者功能: </el-tag>
+        <span style="margin-left: 5px;"> {{ '查看病历、查看药品订单、查看保险报销申请、发起保险报销申请' }}</span>
+      </div><br>
+
+      <div class="mainPageNote">
+        <el-tag type="success">药店功能: </el-tag>
+        <span style="margin-left: 5px;"> {{ '查看病历、创建药品订单、查看药品订单' }}</span>
+      </div><br>
+
+      <div class="mainPageNote">
+        <el-tag>保险机构功能: </el-tag>
+        <span style="margin-left: 5px;"> {{ '查看病历、查看保险报销记录、创建保险报销记录、审核患者发起的报销申请' }}</span>
+      </div><br>
+
+      <div class="mainPageNote">
+        <el-tag type="danger">管理员: </el-tag>
+        <span style="margin-left: 5px;"> {{ '查看各类记录、查看账号、创建账号' }}</span>
+      </div><br>
+
     </el-card>
 
 
@@ -47,7 +73,7 @@ export default {
   }
   .mainPage-card {
     width: 80%;
-    height: 440px;
+    height: 400px;
     margin: 18px auto; /* auto可以使其水平居中 */
   }
 
@@ -57,7 +83,6 @@ export default {
   }
 
   .mainPage-card .mainPageNote {
-    float: left;
     font-size: 16px;
   }
 </style>
