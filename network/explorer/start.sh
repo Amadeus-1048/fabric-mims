3b4373473014f77a114eea8fbe018b1a02d686ef
amadeus-1048 <amadeus1048@outlook.com>
2023-10-29 08:08:40 +0000

    feat: update for Mac & Linux

--- network/explorer/start.sh
+++ network/explorer/start.sh
@@ -4,7 +4,19 @@ priv_sk_path=$(ls ../crypto-config/peerOrganizations/taobao.com/users/Admin\@tao
 
 cp -rf ./connection-profile/network_temp.json ./connection-profile/network.json
 
-sed -i "" "s/priv_sk/$priv_sk_path/" ./connection-profile/network.json
+# 检查操作系统类型
+echo -e "\n"
+echo "检查操作系统类型"
+if [[ `uname` == 'Darwin' ]]; then
+  echo "当前操作系统是 Mac"
+  sed -i "" "s/priv_sk/$priv_sk_path/" ./connection-profile/network.json
+elif [[ `uname` == 'Linux' ]]; then
+  echo "当前操作系统是 Linux"
+  sed -i "s/priv_sk/$priv_sk_path/" ./connection-profile/network.json
+else
+  echo "当前操作系统不是 Mac 或 Linux，脚本无法继续执行！"
+  exit 1
+fi
 
 docker-compose down -v
 docker-compose up -d
